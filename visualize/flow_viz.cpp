f1fa8ff83d50a4fc45922a8a21b3776710ba7ee2
Jzjsnow <5797454@qq.com>
2019-12-24 11:44:36 +0800

    恢复版

--- visualize/flow_viz.cpp
+++ visualize/flow_viz.cpp
@@ -19,9 +19,6 @@ flow_viz::flow_viz(QWidget *parent) :
     _envelope=new OGREnvelope;
     m_originalWidth = this->width();
     m_originalHeight = this->height();
-    od_draw=false;
-    flow_draw=false;
-    map_draw=false;
     //setMouseTracking(true);
 }
 
@@ -31,15 +28,6 @@ flow_viz::~flow_viz()
     delete _flowcollection;
     delete _envelope;
     //delete flowcollection
-}
-
-void flow_viz::set_odcollection(ODcollection *_ocollection)
-{
-    if(_odcollection)
-    {
-        _odcollection=nullptr;
-    }
-    _odcollection=_ocollection;
 }
 
 void flow_viz::set_flowcollection(flowcollection *_fcollection)
@@ -79,7 +67,7 @@ void flow_viz::AutoComputeTransPara()
 void flow_viz::InitialColor()
 {
     base_line=QColor::fromRgb(100,100,100);
-    od_color=QColor::fromRgb(255,215,0);
+    od_color=QColor::fromRgb(47,79,79);
     //base_polygon=QColor::fromRgb(54,78,104);
     base_polygon=QColor::fromRgb(50,50,50);
     /*
@@ -320,19 +308,8 @@ void flow_viz::paintEvent(QPaintEvent *)
     _flowcollection->layerConnection->ResetReading();
     _painter->begin(this);
     _painter->setRenderHint(QPainter::Antialiasing);
-    if(map_draw)
-    {
-        draw_basemap(_painter);
-    }
-    if(od_draw)
-    {
-        draw_OD(_painter);
-    }
-
-    if(flow_draw)
-    {
-        draw_flow(_painter);
-    }
+    draw_basemap(_painter);
+    draw_flow(_painter);
     _painter->end();
     }
     //destroy(_painter);  //"destroys" data on a memory location which makes the object not usable,but the memory is still there for use(the object can be constructed again)
@@ -401,6 +378,7 @@ void flow_viz::wheelEvent(QWheelEvent *event)
         event->accept();
         this->update();
     }
+
 }
 
 void flow_viz::mouseDoubleClickEvent(QMouseEvent *event)
